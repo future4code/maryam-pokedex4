ea719d4cb285baee02f13453f271baa5ffb33850
Tainá Soares <tainasoares10eler@gmail.com>
2021-10-19 17:13:59 -0300

    HomePage with some pokemons and going to DetailsPage

--- pokedex/src/components/CardHome/CardHome.js
+++ pokedex/src/components/CardHome/CardHome.js
@@ -1,9 +1,42 @@
-import React from "react"
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import {useHistory} from "react-router"
+import { goToPage } from '../../Router/coordinator'
+import {CardHomeContainer, ContainerNamePicture, HomeButtons} from "./styled"
 
-export const CardHome = () =>{
+export const CardHome = (props) =>{
+    const history = useHistory()
+
+    const[pokemon, setPokemon] = useState()
+
+    useEffect(()=>{
+        getPokemonPictures(props.url)
+    },[])
+
+    const getPokemonPictures = (url) =>{
+
+        axios.get(url)
+        .then((res)=>{
+            setPokemon(res.data)
+        })
+        .catch((error)=>{
+            console.log(error.response)
+        })
+    }
+
+    
+
+
     return(
-        <div>
-            <p>CardHome</p>
-        </div>
+        <CardHomeContainer>
+            <ContainerNamePicture>
+                <h5>{props.name.toUpperCase()}</h5>
+                <img src={pokemon && pokemon.sprites && pokemon.sprites.versions['generation-v']['black-white'].animated.front_default} alt="Pokemon Picture"/>
+            </ContainerNamePicture>
+            <HomeButtons>
+                <button>Adicionar</button>
+                <button onClick={()=>goToPage(history, "/pokemon/details")}>Detalhes</button>
+            </HomeButtons>
+        </CardHomeContainer>
     )
 }
