06e29dcd0a377bf6c9869fcf9359b18b66b68280
allan-gilber <allangilber@yahoo.com.br>
2021-10-20 02:09:12 -0300

    Made the wireframe of the PokemonDetailsPage.

--- pokedex/src/pages/PokemonDetailsPage.js
+++ pokedex/src/pages/PokemonDetailsPage.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import Imagem from '../img/7.png'
+import { GeneralContainer, Header, ElementContainer, FirstContainer, SecondContainer, ThirdContainer} from './PokemonDetailsPageStyle'
 
 const PokemonDetailsPage = () => {
     return (
         <div>
-            <p>olá!</p>
+            <Header>
+                <button>
+                    Voltar
+                </button>
+
+                <h1>
+                    Nome do Pokemon
+                </h1>
+
+                <button>
+                    Adicionar/Remover da Pokedex
+                </button>
+            </Header>
+            <ElementContainer>
+                <div>
+                <FirstContainer>
+
+                    <img src={Imagem}/>
+                    <img src={Imagem}/>
+
+                </FirstContainer>
+                <SecondContainer>
+                    <h5>Stats</h5>
+                    <p>HP:</p>
+                    <p>attack:</p>
+                    <p>defense:</p>
+                    <p>special-attack:</p>
+                    <p>special-defense:</p>
+                    <p>speed:</p>
+
+                </SecondContainer>
+
+                <ThirdContainer>
+                    <div>
+                        <p>Type1</p>
+                        <p>Type2</p>
+                    </div>
+                    <div>
+                        <h5>Moves</h5>
+                        <p>move name 1</p>
+                        <p>move name 2</p>
+                        <p>move name 3</p>
+
+                    </div>
+
+                </ThirdContainer>
+                </div>
+            </ElementContainer>
         </div>
     )
 }
